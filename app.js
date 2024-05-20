403f327b75f58ac48948928cae1abf28d2512629
Xuo1z <149414772+Xuo1z@users.noreply.github.com>
2024-05-20 20:44:18 +0300

    GG

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const connectToDatabase = require('./database/connect');
+const cors = require('./middlewares/cors');
 
 const gamesRouter = require("./routes/games");
 const categoriesRouter = require("./routes/categories");
 const usersRouter = require("./routes/users");
 
+
 const PORT = 3000;
 
 const app = express();
 
-app.use(bodyParser.json(), express.static(path.join(__dirname, "public")), usersRouter, gamesRouter, categoriesRouter);
+connectToDatabase();
+
+app.use(cors, bodyParser.json(), express.static(path.join(__dirname, "public")), usersRouter, gamesRouter, categoriesRouter);
 
 app.listen(PORT, () => {
     console.log(`Сервер пашит на порту  http://localhost:${PORT}`);
