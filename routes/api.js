0e479d2e6d73c20c9432d1f12218ed12be1c4a0d
Xuo1z <149414772+Xuo1z@users.noreply.github.com>
2024-05-24 21:56:48 +0300

    Добавил авторизацию

--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,11 @@ const apiRouter = require("express").Router();
 const gamesRouter = require("./games.js");
 const categoriesRouter = require("./categories.js");
 const usersRouter = require("./users.js");
+const authRouter = require("./auth.js");
 
 apiRouter.use("/api", usersRouter);
 apiRouter.use("/api", gamesRouter);
 apiRouter.use("/api", categoriesRouter);
+apiRouter.use("/api", authRouter);
 
 module.exports = apiRouter;
